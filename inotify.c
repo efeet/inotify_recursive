ce6ed2d10993a2d3be72dd103aa2624655010337
eFeet VM <hack.efeet@gmail.com>
2015-08-04 06:48:41 -0500

    !!!!!!!!!!!!!!!!!!!!!

--- inotify.c
+++ inotify.c
@@ -49,28 +49,26 @@ static void displayInotifyEvent(struct inotify_event *ev)
     if (ev->cookie > 0)
         logMessage(VB_NOISY, "cookie = %4d; ", ev->cookie);
     
-    if (ev->mask & IN_ISDIR){
+    /*if (ev->mask & IN_ISDIR){
         logMessage(VB_NOISY, "mask = IN_ISDIR ");
 	if (ev->len > 0)
 	  logMessage(VB_NOISY, "Event Name = %s", ev->name);
-    }
+    }*/
 
-    if (ev->mask & IN_CREATE){
+    /*if (ev->mask & IN_CREATE){
         modifiedband = 0;
         logMessage(VB_NOISY, "mask = IN_CREATE ");
 	if (ev->len > 0)
 	  logMessage(VB_NOISY, "Event Name = %s", ev->name);
-    }
+    }*/
 
     if (ev->mask & IN_DELETE_SELF){
-	printf("Borrado1\n");
         logMessage(VB_NOISY, "mask = IN_DELETE_SELF ");
 	if (ev->len > 0)
 	  logMessage(VB_NOISY, "Event Name = %s", ev->name);
     }
     
     if (ev->mask & IN_DELETE){
-	printf("Borrado1.5\n");
         logMessage(VB_NOISY, "mask = IN_DELETE ");
 	if (ev->len > 0)
 	  logMessage(VB_NOISY, "Event Name = %s", ev->name);
@@ -112,11 +110,11 @@ static void displayInotifyEvent(struct inotify_event *ev)
 	  logMessage(VB_NOISY, "Event Name = %s", ev->name);
     }
     
-    if (ev->mask & IN_ATTRIB){
+    /*if (ev->mask & IN_ATTRIB){
         logMessage(VB_NOISY, "mask = IN_ATTRIB ");
 	if (ev->len > 0)
 	  logMessage(VB_NOISY, "Event Name = %s", ev->name);
-    }
+    }*/
     
     //if (ev->mask & IN_OPEN)
         //logMessage(VB_NOISY, "mask = IN_OPEN ");
@@ -614,6 +612,8 @@ static size_t processNextInotifyEvent(int *inotifyFd, char *buf, int bufSize, in
 	modifiedband = 1;
     } else if(ev->mask & IN_MODIFY && modifiedband == 1){
 	modifiedband = 2;
+    } else if(ev->mask & IN_DELETE){
+	modifiedband = 0;
     } else if(ev->mask & IN_CLOSE_WRITE && modifiedband == 2 && showchanges == 1){
 	snprintf(fullPath, sizeof(fullPath), "%s/%s",wlCache[evCacheSlot].path, ev->name);
 	logMessage(0, "Modificacion de contenido en: %s",fullPath);
